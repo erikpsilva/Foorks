818a0bb61a629060c93ddb6e0fed28322863700a
unknown <erik.silva@EP-PROG11.enterplay.local>
2015-10-21 18:11:07 -0200

    arquivos novos

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <title>Foorks</title>
+      <link rel="icon" href="images/favicon.png">
+      <!-- Scripts -->
+      <link rel="stylesheet" type="text/css" href="styles/loader.css">
+      <!-- Scripts -->
+      <script type="text/javascript" src="scripts/jquery.min.js"></script>
+      <script type="text/javascript" src="scripts/modernizr.min.js"></script>
+  </head>
+
+  <body>
+
+    <div class="pre-loader">
+
+    </div>
+
+  </body>
+</html>
