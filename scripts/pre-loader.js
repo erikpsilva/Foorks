ca9457f9e82ded7ccce8e43dd6f3d5d381c00693
unknown <erik.silva@EP-PROG11.enterplay.local>
2015-10-21 17:10:08 -0200

    inclusao de projeto

---
+++ scripts/pre-loader.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  // animacoes da divs junto com transition css3
+  setTimeout(function(){
+    $('.loading').css('width','100%');
+    $('.img-coroa').css('width','100%');
+  }, 300);
+
+  //contagem numerica de 0 a 100
+  var number = 0;
+  var inteval = setInterval(function(){ 
+    number = number + 1
+      $('.loading').html(number  + '%');
+      if ( parseInt(number) == 100){
+            clearInterval(inteval);
+      }
+    }, 48);
+
+
+  //redirecionamento
+    setTimeout(function(){
+    $(window.document.location).attr('href', 'http://www.erikprimao.com.br/');
+  }, 7000);
+
+});
