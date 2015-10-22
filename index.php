ebd002d0b8d130db979116ee18d682bf1e222fc8
Erik Primao <erikpsilva@gmail.com>
2015-10-22 01:38:54 -0200

    pre loader concluido

--- index.php
+++ index.php
@@ -9,13 +9,21 @@
       <!-- Scripts -->
       <script type="text/javascript" src="scripts/jquery.min.js"></script>
       <script type="text/javascript" src="scripts/modernizr.min.js"></script>
+      <script type="text/javascript" src="scripts/pre-loader.js"></script>
   </head>
 
   <body>
 
     <div class="pre-loader">
-
+      <div class="icon-forks-white">
+        <div class="icon-forks transition"></div>
+      </div>
+      <div class="load">
+        <div class="loading transition"></div>
+      </div>
+      <p>web.desenvolvimento.desing</p>
+      <p><span>A solução que precisa está aqui.</span></p>
+      <img src="images/logo-foorks.png" alt="Foorks">
     </div>
-
   </body>
 </html>
--- scripts/pre-loader.js
+++ scripts/pre-loader.js
@@ -3,12 +3,12 @@ $(function(){
   // animacoes da divs junto com transition css3
   setTimeout(function(){
     $('.loading').css('width','100%');
-    $('.img-coroa').css('width','100%');
+    $('.icon-forks').css('width','100%');
   }, 300);
 
   //contagem numerica de 0 a 100
   var number = 0;
-  var inteval = setInterval(function(){ 
+  var inteval = setInterval(function(){
     number = number + 1
       $('.loading').html(number  + '%');
       if ( parseInt(number) == 100){
@@ -19,7 +19,7 @@ $(function(){
 
   //redirecionamento
     setTimeout(function(){
-    $(window.document.location).attr('href', 'http://www.erikprimao.com.br/');
+    //$(window.document.location).attr('href', 'http://www.erikprimao.com.br/');
   }, 7000);
 
 });
